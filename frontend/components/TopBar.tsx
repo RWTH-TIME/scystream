960213a3361cd9b66a17aa511b947ebe07307c7f
PaulKalho <kalhornpaul@gmail.com>
2024-05-22 19:17:00 +0200

    chore: cleanup

---
+++ frontend/components/TopBar.tsx
@@ -0,0 +1,12 @@
+export default function TopBar() {
+  return (
+    <div className="bg-black rounded-b-lg text-white w-screen py-5 px-10 flex flex-row text-lg">
+      <div className="">
+        Breadcrumps
+      </div>
+      <div className="ml-auto uppercase font-bold">
+        scystream
+      </div>
+    </div>
+  )
+}
