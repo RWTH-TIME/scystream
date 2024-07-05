d78313dd2ebf4b0f9a9f884165a20db86c05fd95
PaulKalho <kalhornpaul@gmail.com>
2024-07-05 17:02:51 +0200

    style: use fail early

--- core/src/services/user_service/controllers/auth_controller.py
+++ core/src/services/user_service/controllers/auth_controller.py
@@ -12,34 +12,33 @@ from src.utils.database.session_injector import get_database
 from src.services.user_service.models.user import User
 
 
-"""
-The login function takes email, and password.
-Returns a tuple of access_token and refresh_token
-"""
-
-
 def login(email: str, password: str) -> Tuple[str, str]:
+    """
+    The login function takes email, and password.
+    Returns a tuple of access_token and refresh_token
+    """
+
     db: Session = next(get_database())
     user: User = db.query(User).filter_by(email=email).first()
 
     if not user:
         raise HTTPException(404, detail="User not found")
 
-    if bcrypt.checkpw(password.encode("utf-8"), user.password):
-        # create tokens
-        now: datetime = datetime.now(tz=timezone.utc)
-
-        access_token = create_token({
-            "email": user.email,
-            "iat": now,
-            "exp": now + timedelta(minutes=ENV.JWT_ACCESS_TOKEN_EXPIRE_MIN)
-        })
-
-        refresh_token = create_token({
-            "iat": now,
-            "exp": now + timedelta(days=ENV.JWT_REFRESH_TOKEN_EXPIRE_DAYS)
-        })
-    else:
+    if not bcrypt.checkpw(password.encode("utf-8"), user.password):
         raise HTTPException(401, detail="wrong password")
+
+    # create tokens
+    now: datetime = datetime.now(tz=timezone.utc)
+
+    access_token = create_token({
+        "email": user.email,
+        "iat": now,
+        "exp": now + timedelta(minutes=ENV.JWT_ACCESS_TOKEN_EXPIRE_MIN)
+    })
+
+    refresh_token = create_token({
+        "iat": now,
+        "exp": now + timedelta(days=ENV.JWT_REFRESH_TOKEN_EXPIRE_DAYS)
+    })
 
     return access_token, refresh_token
