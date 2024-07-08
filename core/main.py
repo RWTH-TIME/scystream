daf3f9ab78202649dfc370fbc500d89ee3df7e1c
PaulKalho <kalhornpaul@gmail.com>
2024-07-08 14:31:02 +0200

    feat: remove sqlalchemy inits

--- core/main.py
+++ core/main.py
@@ -5,12 +5,6 @@ from contextlib import asynccontextmanager
 
 from services.user_service.views import user as user_view
 
-
-@asynccontextmanager
-async def lifespan(_: FastAPI):
-    Base.metadata.create_all(bind=engine)
-    yield
-
-app = FastAPI(title="scystream-core", lifespan=lifespan)
+app = FastAPI(title="scystream-core")
 
 app.include_router(user_view.router)
