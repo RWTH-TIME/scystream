a5b47f1909e70e2ea4495550890ac30cf6c99b53
mottegk <mottegk@web.de>
2024-08-05 09:52:50 +0200

    wip: add endpoint to refresh access_token

--- core/utils/helper/jwt.py
+++ core/utils/helper/jwt.py
@@ -2,6 +2,7 @@ import jwt
 from utils.config.environment import ENV
 
 
+
 def create_token(payload: dict) -> str:
     """
     wrapper function for jwt.encode creating jwt tokens with our secret and
@@ -12,3 +13,19 @@ def create_token(payload: dict) -> str:
         ENV.JWT_SECRET,
         algorithm=ENV.JWT_ALGORITHM
     )
+
+
+def verify_token(token: str) -> dict: 
+    """
+    wrapper function for jwt.decode, verifying jwt tokens with our secret and
+    algorithm, returns the payload
+    """
+    payload= jwt.decode(
+            token,
+            ENV.JWT_SECRET,
+            algorithm=ENV.JWT_ALGORITHM
+        )
+    if not payload:     #might be unnecessary
+        raise jwt.InvalidSignatureError
+    
+    return payload
