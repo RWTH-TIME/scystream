8249eec25c8463f3213411116344b026cac6b172
PaulKalho <kalhornpaul@gmail.com>
2024-08-14 13:00:42 +0200

    fix: remove testing cors

--- core/main.py
+++ core/main.py
@@ -1,16 +1,7 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 from services.user_service.views import user as user_view
 
 app = FastAPI(title="scystream-core")
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 app.include_router(user_view.router)
