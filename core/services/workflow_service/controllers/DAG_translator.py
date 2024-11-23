55eee7991c15520465df4619f62ce3bb99d55969
mottegk <mottegk@web.de>
2024-11-23 16:21:01 +0100

    wip: DAG translator

--- core/services/workflow_service/controllers/DAG_translator.py
+++ core/services/workflow_service/controllers/DAG_translator.py
@@ -1,18 +1,28 @@
 import os
 from jinja2 import Environment, FileSystemLoader
-from sqlalchemy.orm import Session
 from fastapi import HTTPException
 import networkx as nx
 from typing import Dict, Any
 
-from models.project import Project
 from project_controller import read_project
 # from models.block import Block
 
+# from enum import Enum
+# from json import loads
+# from typing import NoReturn, List, Optional
 
-def parse_project_to_dag(project_uuid: str) -> Dict[str, Any]:
+# from airflow_client.client import ApiClient, Configuration
+# from airflow_client.client.api.dag_api import DAGApi
+# from airflow_client.client.api.dag_run_api import DAGRunApi
+# from airflow_client.client.exceptions import NotFoundException
+# from airflow_client.client.model.dag import DAG
+# from airflow_client.client.model.dag_run import DAGRun
+# from airflow_client.client.model.dag_state import DagState
+
+
+def translate_project_to_dag(project_uuid: str) -> Dict[str, Any]:
     """
-    Parses a project and its blocks into a DAG.
+    Parses a project and its blocks into a DAG and validates it.
 
     :param project_uuid: UUID of the project to parse.
     :param db: SQLAlchemy database session.
@@ -20,22 +30,16 @@ def parse_project_to_dag(project_uuid: str) -> Dict[str, Any]:
     # Query the project
     project = read_project(project_uuid)
 
-    # Initialize Jinja2 environment
-    env = Environment(loader=FileSystemLoader("templates"))
-    dag_template = env.get_template("dag_base.py.j2")
-    algorithm_template = env.get_template("algorithm_docker.py.j2")
-    dependency_template = env.get_template("dependency.py.j2")
-
     # Create a directed graph
     graph = nx.DiGraph()
 
     # Add nodes (tasks)
     for block in project.blocks:
-        task_id = f"task_{str(block.uuid).replace('-', '')}"
-        graph.add_node(task_id, **{
+        graph.add_node(block.uuid, **{
             "name": block.name,
             "type": block.block_type.value,
             "parameters": block.parameters,
+            "priority": block.priority_weight,
             "retries": block.retries,
             "retry_delay": block.retry_delay,
             "environment": block.environment,
@@ -54,10 +58,24 @@ def parse_project_to_dag(project_uuid: str) -> Dict[str, Any]:
         raise HTTPException(
             status_code=400,
             detail="The project is not acyclic."
+            )
+
+    # Initialize Jinja2 environment
+    base_dir = os.path.dirname(os.path.abspath(__file__))  
+    templates_dir = os.path.join(  # Path to the templates
+        base_dir, "..", "templates"
         )
 
+    env = Environment(loader=FileSystemLoader(templates_dir))
+    dag_template = env.get_template("dag_base.py.j2")
+    algorithm_template = env.get_template("algorithm_docker.py.j2")
+    dependency_template = env.get_template("dependency.py.j2")
+
     # Generate Python DAG file
-    parts = [dag_template.render(dag_id=f"dag_{project_uuid.replace('-', '_')}")]
+    parts = []
+    parts = [dag_template.render(
+        dag_id=f"dag_{project_uuid.replace('-', '_')}"
+        )]
     dependencies = []
 
     # Convert to Airflow-compatible representation
@@ -65,30 +83,30 @@ def parse_project_to_dag(project_uuid: str) -> Dict[str, Any]:
         parts.append(
             algorithm_template.render(
                 task_id=node,
-                image=ConfigEntry.WORKER_DOCKER_IMAGE.value,
+                image="scystreamworker",
                 name=data["name"],
                 uuid=node.split("_", 1)[1],
                 dependency="",
                 project=str(project_uuid),
-                pipeline=str(project.pipeline_uuid),
+                # pipeline=str(project.pipeline_uuid),
                 algorithm=data["type"],
                 parameters=data["parameters"],
-                storage_driver=ConfigEntry.STORAGE_DRIVER.value,
-                aws_access_key_id=ConfigEntry.AWS_ACCESS_KEY_ID.value,
-                aws_secret_access_key=ConfigEntry.AWS_SECRET_ACCESS_KEY.value,
-                aws_region=ConfigEntry.AWS_REGION.value,
-                local_storage_path_external=ConfigEntry.LOCAL_STORAGE_PATH_EXTERNAL.value,
+                local_storage_path_external="/tmp/scystream-data",
                 container_name=project.container_name,
             )
         )
 
     for from_task, to_task in graph.edges:
-        dependencies.append(dependency_template.render(from_task=from_task, to_task=to_task))
+        dependencies.append(dependency_template.render(
+            from_task=from_task, to_task=to_task)
+            )
 
     parts.extend(dependencies)
 
     # Write the generated DAG to a Python file
-    filename = os.path.join(ConfigEntry.AIRFLOW_DAGS.value, f"dag_{project_uuid.replace('-', '_')}.py")
+    filename = os.path.join(
+        "~/airflow/dags/", f"dag_{project_uuid.replace('-', '_')}.py"
+        )
 
     with open(filename, "w") as f:
         f.write("\n".join(parts))
