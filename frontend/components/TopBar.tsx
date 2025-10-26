478aff10300f66ab884a5be94e05667e92231de0
PaulKalho <kalhornpaul@gmail.com>
2025-10-26 17:58:11 -0600

    feat: add logout button

--- frontend/components/TopBar.tsx
+++ frontend/components/TopBar.tsx
@@ -3,6 +3,7 @@ import BreadCrumbs from "./BreadCrumbs"
 
 import { useEffect, useState } from "react"
 import { getUser, logout } from "@/utils/auth/authService"
+import { Logout } from "@mui/icons-material"
 
 export type TopBarProps = Partial<BreadCrumbsProps>
 
@@ -12,24 +13,30 @@ export default function TopBar({ breadcrumbs }: TopBarProps) {
   useEffect(() => {
     async function fetchUser() {
       const fetchedUser = await getUser()
-
-      if (fetchedUser != undefined && fetchedUser.profile && fetchedUser.profile.name) {
+      if (fetchedUser?.profile?.name) {
         setUser(fetchedUser.profile.name)
       }
     }
-
     fetchUser()
   }, [])
 
   return (
     <div className="bg-black items-center text-white w-screen py-5 px-10 flex flex-row text-lg h-[80px]">
-      <div className="">
-        <BreadCrumbs breadcrumbs={breadcrumbs ? breadcrumbs : []} />
+      {/* Breadcrumbs */}
+      <div>
+        <BreadCrumbs breadcrumbs={breadcrumbs ?? []} />
       </div>
-      <div className="flex flex-row items-center ml-auto uppercase gap-x-1.5 tracking-wide cursor-pointer">
-        <a onClick={() => logout()}>
-          {user}
-        </a>
+
+      {/* User section */}
+      <div className="flex flex-row items-center ml-auto uppercase tracking-wide">
+        <span className="cursor-default">{user}</span>
+        <button
+          onClick={() => logout()}
+          className="ml-4 p-1 text-red-500 hover:text-red-400 transition-colors cursor-pointer"
+          title="Logout"
+        >
+          <Logout fontSize="medium" />
+        </button>
       </div>
     </div>
   )
